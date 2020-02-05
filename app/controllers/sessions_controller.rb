ea6ec5abdd4d7d882eb711968feef4675f6ddedf
addo <forison6205@gmail.com>
2020-02-05 06:35:29 -0500

    added jwt token for encoding and decoding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:email])
-		if User.exists?(email: params[:email])
-			# sign in  user
-			render json: user, status: :ok
+		if user&.authenticate(params[:password])
+			token = JsonWebToken.encode(user_id: user.id)
+			time = Time.now + 24.hours.to_i
+			render json: {token: token, time: time, user_id: user.id}, status: :ok
 		else
 			render json: params[:email], status: :unauthorized
 		end
   end
 	def destroy 
 	end
-
 end
