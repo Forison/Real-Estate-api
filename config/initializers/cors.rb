c3160f16efc915251a50743a6811f2837d15e38c
addo <forison6205@gmail.com>
2020-02-10 10:29:51 -0500

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000', '127.0.0.1:3000',
+            /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
 
     resource '*',
       headers: :any,
