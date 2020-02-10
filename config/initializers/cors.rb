11ccbc3a82ce681d62fa80ac335215d609a7634b
addo <forison6205@gmail.com>
2020-02-10 11:03:44 -0500

    added credential params to cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000',
-            /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+    origins 'localhost:3000'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
