2809b538ca3c7a369da13f6bb38b46461f04561e
addo <forison6205@gmail.com>
2020-02-27 08:46:39 -0500

    modified cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000/ ,http://bfaddo.com/'
 
     resource '*',
              headers: :any,
