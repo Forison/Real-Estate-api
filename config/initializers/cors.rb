48a221077c26f2b71bc57b086a5f7908a3533614
addo <forison6205@gmail.com>
2020-02-28 03:50:45 -0500

    reset origin of cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000/ ,http://bfaddo.com/'
-
+    origins 'http://localhost:3000'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
