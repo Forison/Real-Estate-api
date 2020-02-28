f6b570d6cfd392b7997f8de09b995250f1d29a9e
addo <forison6205@gmail.com>
2020-02-28 06:43:00 -0500

    refactore get user path to seller path

--- app/controllers/get_users_controller.rb
+++
@@ -1,7 +0,0 @@
-class GetUsersController < ApplicationController
-  before_action :authorize_request
-  def show
-    get_user = User.find(params[:id])
-    render json: get_user, status: :ok
-  end
-end
---
+++ app/controllers/seller_controller.rb
@@ -0,0 +1,7 @@
+class SellerController < ApplicationController
+  before_action :authorize_request
+  def show
+    uploader = User.find(params[:id])
+    render json: uploader, status: :ok
+  end
+end
