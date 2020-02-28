456fcdd7142b15895ddb3872e857d470c74ccc05
addo <forison6205@gmail.com>
2020-02-28 03:56:32 -0500

    refactored cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000, http://bfaddo.com/'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
