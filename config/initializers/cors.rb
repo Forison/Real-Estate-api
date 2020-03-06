bbeecc28bb9adf2ba97c1c9f1d4d68f961e1b539
addo <forison6205@gmail.com>
2020-03-06 06:56:42 -0500

    set cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://frozen-bastion-98066.herokuapp.com/signUp'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
