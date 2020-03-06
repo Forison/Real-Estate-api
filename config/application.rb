50d6d7238345eb52886a39d67cbbb4d14114e314
addo <forison6205@gmail.com>
2020-03-06 14:08:46 -0500

    set rack cors config in rails appliction controller3

--- config/application.rb
+++ config/application.rb
@@ -25,12 +25,6 @@ module RealEstateApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://frozen-bastion-98066.herokuapp.com/'
-        resource '*', headers: :any, methods: %i[get post options]
-      end
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'https://frozen-bastion-98066.herokuapp.com/'
-#     resource '*',
-#              headers: :any,
-#              methods: %i[get post put patch delete options head],
-#              credentials: true
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://frozen-bastion-98066.herokuapp.com/'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put ],
+             credentials: true
+  end
+end
