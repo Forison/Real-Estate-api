50449f83c40883339fe773a308613098e1ed013f
addo <forison6205@gmail.com>
2020-03-06 05:16:07 -0500

    reset cors to allow access from all origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://frozen-bastion-98066.herokuapp.com'
+    origins '*'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
