e2a4ae6b342b5825644568e61e3b9ee439b8abb3
addo <forison6205@gmail.com>
2020-03-09 07:42:30 -0400

    added as seller

--- app/controllers/currents_controller.rb
+++ app/controllers/currents_controller.rb
@@ -17,8 +17,9 @@ class CurrentsController < ApplicationController
   end
 
   def user_homes
-    render json: { isLogin: @current_user.houses }, status: :ok
+    render json: { asSeller: @current_user.houses }, status: :ok
   end
+
   def is_user_login?
     if @current_user
       render json: { isLogin: true }, status: :ok
