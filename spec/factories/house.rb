4c3a43e964f26766e27f5dd87eac6aa4da079b87
addo <forison6205@gmail.com>
2020-03-13 11:15:18 -0400

    added validations unit testing for user and house model

---
+++ spec/factories/house.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :house do
+    name { 'MyString' }
+    category { 'some category' }
+    description { 'some category' }
+    price { '12222222' }
+    location { 'Ghana' }
+		sequence(:user_id) { |n| n }
+		pictures {['/spec/support/assets/test.png']}    
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
-  factory :userdo
+  factory :user do
     username { 'addo' }
     country { 'kofi' }
     sequence(:email) { |n| "tester#{n}@example.com" }
-    password { 'ammorethansix' }
-    trait :with_avatar do
-      avatar { Rack::Test::UploadedFile.new(Rails.root.to_s + '/spec/support/assets/test.jpg', 'img/jpg') }
-    end
+    password { 'iammorethansix' }
+    picture {'/spec/support/assets/test.png'}
   end
 end
