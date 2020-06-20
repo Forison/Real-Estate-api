d54f363377c56f3285f9dc1b79b49f89c11c6f5e
forison <forison6205@gmail.com>
2020-06-19 20:08:56 -0400

    fix favorites bug index action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,9 @@ class FavoritesController < ApplicationController
   before_action :authorize_request
   
   def index
-    render json: @current_user.favorites.ids, status: :ok
+    render json: @current_user.favorites, status: :ok
   end
-  
+
   def create
     favorites = @current_user.favorites.build(fav_params)
     if favorites.save
