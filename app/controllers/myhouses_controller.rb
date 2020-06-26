f4a2da879287bb85fae69adb91bbd5408a085650
forison <addoboakyeforison@gmail.com>
2020-06-25 22:30:12 -0400

    add redis gem

--- app/controllers/myhouses_controller.rb
+++
@@ -1,9 +0,0 @@
-class MyhousesController < ApplicationController
-  def favorite
-    house = House.find([params[:id]])
-    render json: params[id], status: :ok
-  end
-  def query_params
-    params.permit(id: [])
-  end
-end
