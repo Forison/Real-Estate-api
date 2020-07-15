4b74926684e3280298345b7e551d15163b08aa0e
forison <addoboakyeforison@gmail.com>
2020-07-15 16:11:15 -0400

    set default value for read attribute

--- db/migrate/20200715200302_create_notifications.rb
+++ db/migrate/20200715200302_create_notifications.rb
@@ -3,7 +3,7 @@ class CreateNotifications < ActiveRecord::Migration[6.0]
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
       t.references :notifyable, polymorphic: true, null: false
-      t.boolean :read
+      t.boolean :read, default: false
 
       t.timestamps
     end
