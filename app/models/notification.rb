c171c63fada615878527d702b4665cafe10cb87b
forison <addoboakyeforison@gmail.com>
2020-07-15 16:07:34 -0400

    add validation to notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notifyable, polymorphic: true
+  validates_uniqueness_of :notifyable_type, scope: :notifyable_id, if: Proc.new{ |atts| atts.notifyable_id.present? }
+
+  after_create_commit { BroadcastNotificationJob.perform_later(self.id) }
 end
