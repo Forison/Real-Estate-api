d489abd9dca03ff3ffe3528f7e3ff8e6f1141b21
forison <addoboakyeforison@gmail.com>
2020-07-21 12:07:57 -0400

    notification correction 1

--- app/models/house.rb
+++ app/models/house.rb
@@ -7,11 +7,12 @@ class House < ApplicationRecord
   validates :location, presence: true
   validates :pictures, presence: true
 
-  after_create_commit { house_notification(self.id) } 
+  # after_create_commit { house_notification(self.id) }
+  after_create_commit { NotificationBroadcastJob(self.id) } 
 
-  def house_notification(house_id)
-    houses = House.find_by_id(house_id)
-    return unless houses.present?
-    Houseupdate.create!(poster: houses.user_id, item: house_id, reader: nil, status: nil)
-  end
+  # def house_notification(house_id)
+  #   houses = House.find_by_id(house_id)
+  #   return unless houses.present?
+  #   Houseupdate.create!(poster: houses.user_id, item: house_id, reader: nil, status: nil)
+  # end
 end
